d8bc9d17809efaa8fc9154c3fe4f0723f036d685
Sithum Dissanayake <sithum_d@outlook.com>
2018-05-13 20:28:32 +1000

    13/5

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,24 @@
-/* Code for smooth scrolliing from: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
 $(document).ready(function(){
-  $(".navbar a, footer a[href='#']").on('click', function(event) {
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-  if (this.hash !== "") {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-    event.preventDefault();
+      // Store hash
+      var hash = this.hash;
 
-    var hash = this.hash;
-
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-
-      window.location.hash = hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
       });
-    } 
+    } // End if
   });
-})
-
-var myVar;
-
-function myFunction() {
-    myVar = setTimeout(showPage, 1000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("page").style.display = "block";
-}
-
+});
